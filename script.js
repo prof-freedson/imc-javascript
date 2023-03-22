73d68744f0dd42a21d761d571b9c4518610a1b41
prof-freedson <freedson.prof@gmail.com>
2023-03-22 19:54:18 -0300

    Comentários adicionados

---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+
+Elaborar a função
+
+- declarar variáveis
+
+- estrutura condicional
+    - imc < 19 : magreza
+    - imc >= 19 e imc < 24 : peso normal
+    - imc >= 24 e imc < 29 : acima do peso
+    - imc >= 29 : obesidade
+
+- exibir o resultado e a imagem correspondente
+
+*/
