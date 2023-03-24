c6a7e223ac8445f6e287deab710a914fca001f8d
ricardo-dsm <ricardo.dsmdev@gmail.com>
2023-03-24 20:36:42 -0300

    Indicação do Peso

--- script.js
+++ script.js
@@ -19,16 +19,22 @@ function calcIMC() {
     var altura = document.getElementById('altura').value
     var imc = document.getElementById('imc')
     var imagem = document.getElementById('imagem')
+    var sit = document.getElementById('sit')
     var res = peso / (altura * altura)
 
+    
     if (res < 19) {
         imagem.innerHTML = `<img src="magreza.png">`
+        sit.innerHTML = '<h1>Magreza<h1>'
     } else if (res >= 19 && res < 24) {
         imagem.innerHTML = `<img src="normal.png">`
+        sit.innerHTML = '<h1>Peso Normal<h1>'
     } else if (res >= 24 && res < 29) {
         imagem.innerHTML = `<img src="sobrepeso.png">`
+        sit.innerHTML = '<h1>Sobrepeso<h1>'
     } else {
         imagem.innerHTML = `<img src="obesidade.png">`
+        sit.innerHTML = '<h1>Obesidade<h1>'
     }
         imc.innerHTML = `<h1>Resultado do IMC: ${res}</h1>`
 }
